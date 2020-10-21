9652587ced33ff2490844f4f091313e274b8b1ce
matiasdaloia <matidaloia@gmail.com>
2020-10-21 15:20:13 -0300

    finished mundo impresiones back end

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '276dc3733774ab143d9c1a9717ea4497'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f41a2963-bbf5-41ef-a371-fcac7d614113'
+};
