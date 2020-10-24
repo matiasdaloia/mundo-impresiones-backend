6996ba1262da47269ea3bcaeb858e1df42576871
matiasdaloia <matidaloia@gmail.com>
2020-10-24 13:17:10 -0300

    added cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("dcj3s6q8s"),
+      api_key: env("632188672722564"),
+      api_secret: env("-HbMNOn7r9-ZbNTvZ8_Y1B79tZI"),
+    },
+  },
+  // ...
+});
