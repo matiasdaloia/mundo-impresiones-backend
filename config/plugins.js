e21d249815af866e075851b634206d468e303869
matiasdaloia <matidaloia@gmail.com>
2020-10-24 13:28:56 -0300

    fixed cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "cloudinary",
     providerOptions: {
-      cloud_name: env("dcj3s6q8s"),
-      api_key: env("632188672722564"),
-      api_secret: env("-HbMNOn7r9-ZbNTvZ8_Y1B79tZI"),
+      cloud_name: env("CLOUD_NAME"),
+      api_key: env("API_KEY_CLOUDINARY"),
+      api_secret: env("API_SECRET"),
     },
   },
   // ...
